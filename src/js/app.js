d8f74c11097de971bf3157e77568113c0be8700d
José Antonio <jose_antoniorivera@outlook.com>
2025-06-14 19:44:24 -0600

    refactor: change the way to save data, now the list that is saved in   the storage contains a string in json format

--- src/js/app.js
+++ src/js/app.js
@@ -1,78 +1,76 @@
+$(document).ready(function () {
 
-$(document).ready( function () {
-  
-  showToDoList();
+    const TASK_STORAGE_KEY = "TASK_STORAGE_KEY";
 
-  $('#createTaskButton').on('click', function () {
+    showToDoList();
 
-    let todo = [
-            document.querySelector("#task-title").value,
-            document.querySelector("#task-description").value
-          ];
+    $('#createTaskButton').on('click', function () {
 
-    if(Validator.isValueFieldValid(todo[0]) && Validator.isValueFieldValid(todo[1])) {
+        let taskTitle = document.querySelector('#task-title').value;
+        let taskDescription = document.querySelector('#task-description').value;
 
-      $('.table-list-todo').hide(500, function () {
-            let todoList = localStorage.getItem("todoStorage") ? JSON.parse(localStorage.getItem("todoStorage")) : [];
-            todoList.push(todo);
-            localStorage.setItem("todoStorage", JSON.stringify(todoList));
-            console.table( todoList );
-            console.log("Se ha guardado exitosamente.");
+        let task = new Task(taskTitle, taskDescription);
+
+        if (Validator.isTaskValid(task)) {
+
+            $('.table-list-todo').hide(500, function () {
+
+                let storageTaskList = localStorage.getItem(TASK_STORAGE_KEY) ? JSON.parse(localStorage.getItem(TASK_STORAGE_KEY)) : [];
+
+                storageTaskList.push(task.toJson());
+
+                localStorage.setItem(TASK_STORAGE_KEY, JSON.stringify(storageTaskList));
+
+                console.table(storageTaskList);
+                console.log("Task was saved successfully.");
+
+                showToDoList();
+                $('.table-list-todo').show(500);
+
+            });
+
+        } else {
+
+            alert("All fields are required to create a task");
+
+        }
+
+    });
+
+    $('#cleanButton').on('click', function () {
+
+        $('.table-list-todo').hide(500, function () {
+
+            localStorage.removeItem(TASK_STORAGE_KEY);
+
             showToDoList();
-            $('.table-list-todo').show(500);
-          });
 
-    } else {
+            $('.table-list-todo').show(1000);
 
-      alert("All fields are required to create a task");
+        });
 
+    });
+
+    function showToDoList() {
+
+        let storageTaskList = localStorage.getItem(TASK_STORAGE_KEY) ? JSON.parse(localStorage.getItem(TASK_STORAGE_KEY)) : [];
+
+        let template = `<thead>
+                                    <tr><td>Name</td><td>Description</td></tr>
+                               </thead>
+                               <tbody>`;
+
+        storageTaskList.forEach(rawTask => {
+
+            let task = Task.toTask(rawTask);
+
+            template += `<tr><td>${task.getTitle}</td><td>${task.getDescription}</td></tr>`;
+
+        });
+
+        template += `</tbody>`;
+
+        $('.table-list-todo').html(template);
     }
 
-  });
-
-  $('#cleanButton').on('click', function () {
-    $('.table-list-todo').hide(500, function () {
-      localStorage.removeItem("todoStorage");
-      showToDoList();
-      $('.table-list-todo').show(1000);
-    });
-  });
-
-  function showToDoList () {
-    let todoList = localStorage.getItem("todoStorage") ? JSON.parse(localStorage.getItem("todoStorage")) : [];
-    
-    let template = `
-    <thead>
-      <tr><td>Name</td><td>Description</td></tr>
-    </thead>
-    <tbody>
-    `;
-    
-    todoList.forEach(todo => {
-      template += `<tr><td>${ todo[0] }</td><td>${ todo[1] }</td></tr>`;
-    });
-    
-    template += `</tbody>`;
-    
-    $('.table-list-todo').html(template);
-  }
-
 });
-
-
-
-
-// const button = document.querySelector("button");
-
-// button.addEventListener("click", (event) => {
-//   button.textContent = `Click count: ${event.detail}`;
-// });
-
-// const boxWrapper = document.getElementById("box-wrapper");
-
-// const box = document.createElement("div");
-// box.innerHTML = 'div <br> 5';
-// box.style.backgroundColor = "orange";
-// box.classList.add("box");
-
-// boxWrapper.appendChild(box);
